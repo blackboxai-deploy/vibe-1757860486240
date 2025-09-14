e31ad6151d6906d6ab22ea50d4ea9e41b88e7668
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-14 20:04:54 +0530

    Add src/app/page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import QuickSortVisualizer from '@/components/QuickSortVisualizer';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      <QuickSortVisualizer />
+    </div>
+  );
+}
